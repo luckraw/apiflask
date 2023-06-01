ac9d473e4cc52e4953e8c5dfe48dfebf8b9bfb89
luckraw <krawdev@gmail.com>
2023-06-01 14:31:28 -0300

    update routes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-postagens = [
+post = [
     {'title': 'evangelho de sangue',
      'autor': 'clive barker'
      },
@@ -17,36 +17,36 @@ postagens = [
 ]
 
 @app.route('/')
-def obter_postagens():
-    return jsonify(postagens)
+def post():
+    return jsonify(post)
 
-@app.rout('/postagens', methods=['GET'])
-def obter_postagens():
-    return jsonify(postagens)
+@app.route('/post', methods=['GET'])
+def get_post():
+    return jsonify(post)
 
-@app.route('/postagens/<int:indice>', methods=['GET'])
-def obter_postagens_porindice(indice):
-    return jsonify(postagens[indice])
+@app.route('/post/<int:indice>', methods=['GET'])
+def get_post_by_indice(indice):
+    return jsonify(post[indice])
 
-@app.route('/postagem', methods=['POST'])
-def fazer_postagem():
-    dados = request.get_json()
-    dados.append(dados)
-    return jsonify(dados, 200)
+@app.route('/post', methods=['POST'])
+def create_post():
+    data = request.get_json()
+    data.append(data)
+    return jsonify(data, 200)
 
-@app.route('/postegem/<int:indice>', methods=['PUT'])
-def alterar_postagem(indice):
-    postagem_alterada = request.get_json()
-    postagens[indice].update(postagem_alterada)
-    return jsonify(postagens[indice], 200)
+@app.route('/post/<int:indice>', methods=['PUT'])
+def change_post(indice):
+    post_alter = request.get_json()
+    post[indice].update(post_alter)
+    return jsonify(post[indice], 200)
 
-@app.doute('/postagem/<int:indice>', methods=['DELETE'])
-def excluir_postagem(indice):
+@app.route('/post/<int:indice>', methods=['DELETE'])
+def delete_post(indice):
     try:
-        if postagens[indice] is not None:
-            del postagens[indice]
-            return jsonify(f'Postagem excluída com sucesso. {postagens[indice]}', 200)
+        if post[indice] is not None:
+            del post[indice]
+            return jsonify(f'Post deleted successfully. {post[indice]}', 200)
     except:
-        return jsonify('Não foi possível encontrar a postagem para exclusão', 404)
+        return jsonify('Could not find post for deletion', 404)
 
 app.run(port=7777, host='localhost', debug=True)
